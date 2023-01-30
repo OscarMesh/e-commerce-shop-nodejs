aa9db7992afde8159591f62b5d7fee0e97b07855
osacrMesh <okerekeifeanyi65@gmail.com>
2023-01-30 16:25:53 +0100

    clean

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,18 +1,20 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const stripeClient = require("stripe")(process.env.STRIPE_SECRET_KEY_MY);
 
 router.post("/payment", (req, res) => {
-  stripe.charges.create(
+  stripeClient.charges.create(
     {
-      source: req.body.token.id,
+      source: req.body.tokenId,
       amount: req.body.amount,
       currency: "usd",
     },
     (stripeErr, stripeRes) => {
       if (stripeErr) {
-        res.status(500).send({ error: stripeErr });
+        res.status(500).json(stripeErr);
       } else {
-        res.status(200).send({ success: stripeRes });
+        res.status(200).json(stripeRes);
       }
     }
   );
