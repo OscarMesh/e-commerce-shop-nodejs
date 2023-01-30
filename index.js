de06b5fe7f460df0769a28d1c1351ab8041200b1
osacrMesh <okerekeifeanyi65@gmail.com>
2023-01-30 17:16:01 +0100

    live

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
